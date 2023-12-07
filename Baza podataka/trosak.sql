79ba942b35e9c210ff2e926e6186115cb9181675
STRAHINJA\Korisnik <ms180555d@student.etf.bg.ac.rs>
2023-12-07 14:19:37 +0100

    Brisanje svega

--- Baza podataka/trosak.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM stambena_zajednica.trosak
-LIMIT 0, 1000
-
--- Date: 2023-12-06 18:13
-*/
-INSERT INTO `trosak` (`id`,`iznos`,`datum`,`opis`) VALUES (1,2000,'2023-12-10','Trosak popravke...');
-INSERT INTO `trosak` (`id`,`iznos`,`datum`,`opis`) VALUES (2,3000,'2023-12-12','Trosak ciscenja');
