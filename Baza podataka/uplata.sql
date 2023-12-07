85bd5f818a6daf328bb9b6738e68ef273d7d5724
STRAHINJA\Korisnik <ms180555d@student.etf.bg.ac.rs>
2023-12-07 14:21:37 +0100

    Inicijalni commit baze podataka

---
+++ Baza podataka/uplata.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM stambena_zajednica.uplata
+LIMIT 0, 1000
+
+-- Date: 2023-12-06 18:00
+*/
+INSERT INTO `uplata` (`id`,`korisnikId`,`iznos`,`datum`,`trenutnoStanje`,`tip`) VALUES (1,1,1500,'2023-12-5',0,0);
+INSERT INTO `uplata` (`id`,`korisnikId`,`iznos`,`datum`,`trenutnoStanje`,`tip`) VALUES (2,2,1500,'2023-12-5',0,1);
