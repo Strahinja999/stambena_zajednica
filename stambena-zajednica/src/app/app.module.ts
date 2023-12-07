c86c022171b9894da5f61228aa1a9db154718c5f
STRAHINJA\Korisnik <ms180555d@student.etf.bg.ac.rs>
2023-12-07 18:53:46 +0100

    Front prijava

--- stambena-zajednica/src/app/app.module.ts
+++ stambena-zajednica/src/app/app.module.ts
@@ -3,15 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrijavaComponent } from './prijava/prijava.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import { UpravnikComponent } from './upravnik/upravnik.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrijavaComponent,
+    UpravnikComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
